9c2d477b1d9bc382d9c5db03c20a294cee34c04c
jidelson <58210030+jidelson@users.noreply.github.com>
2020-04-08 11:04:46 -0700

    just need nav links

--- js/index.js
+++ js/index.js
@@ -40,3 +40,80 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+let midImg = document.getElementById("middle-img");
+midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+const navis = document.querySelectorAll('nav a');
+navis[0].textContent = siteContent["nav"]["nav-item-1"];
+navis[1].textContent = siteContent["nav"]["nav-item-2"];
+navis[2].textContent = siteContent["nav"]["nav-item-3"];
+navis[3].textContent = siteContent["nav"]["nav-item-4"];
+navis[4].textContent = siteContent["nav"]["nav-item-5"];
+navis[5].textContent = siteContent["nav"]["nav-item-6"];
+
+
+const h1s = document.querySelectorAll('h1');
+h1s[0].textContent = siteContent["cta"]["h1"];
+
+const buttons = document.querySelectorAll('button');
+buttons[0].textContent = siteContent["cta"]["button"];
+
+const h4s = document.querySelectorAll('h4');
+h4s[0].textContent = siteContent["main-content"]["features-h4"];
+h4s[1].textContent = siteContent["main-content"]["about-h4"];
+h4s[2].textContent = siteContent["main-content"]["services-h4"];
+h4s[3].textContent = siteContent["main-content"]["product-h4"];
+h4s[4].textContent = siteContent["main-content"]["vision-h4"];
+h4s[5].textContent = siteContent["contact"]["contact-h4"];
+
+const words = document.querySelectorAll('p');
+words[0].textContent = siteContent["main-content"]["features-content"];
+words[1].textContent = siteContent["main-content"]["about-content"];
+words[2].textContent = siteContent["main-content"]["services-content"];
+words[3].textContent = siteContent["main-content"]["product-content"];
+words[4].textContent = siteContent["main-content"]["vision-content"];
+words[5].textContent = siteContent["contact"]["address"];
+words[6].textContent = siteContent["contact"]["phone"];
+words[7].textContent = siteContent["contact"]["email"];
+words[8].textContent = siteContent["footer"]["copyright"];
+
+const navColor = document.querySelectorAll('nav a');
+navColor[0].style.color = 'green';
+navColor[1].style.color = 'green';
+navColor[2].style.color = 'green';
+navColor[3].style.color = 'green';
+navColor[4].style.color = 'green';
+navColor[5].style.color = 'green';
+
+/*const newLink = document.createElement('href');
+newLink.href = "https://www.google.com/";
+document.siteContent.nav.appendChild(newLink);
+
+const addNavigation = document.querySelectorAll('nav');
+addNavigation.href = "https://www.google.com/";
+nav.appendChild(addNavigation);
+
+const newLink = document.createElement('href');
+const node = document.createTextNode("Google");
+newLink.href = "https://www.google.com/";
+newLink.appendChild(node);
+
+
+const node = document.getElementsByTagName('nav a');
+const nodeChild = document.createElement('a');
+nodeChild.href = "https://www.google.com/";
+node.appendChild(nodeChild);
+
+
+const link = document.createElement('a');
+link.setAttribute('href', 'https://www.google.com/');
+link.setAttribute('name', 'Google');
+*/
+var newNav = document.createElement("nav");
+var newA = document.createElement("a");
+newNav.appendChild(newA);
+newA.innerHTML = "Google";
